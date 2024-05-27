f6aab5933b8578ed0aacc4118c75e00dd47b4940
Hope2925 <hopekirby2@gmail.com>
2024-05-27 14:30:56 -0600

    edited sbatch scripts to be easier to follow and to have some annotation tasks
    Former-commit-id: 91fe200bb3bee057b4a32384d0dacd62b7f98c79

---
+++ day05/scripts/run_d5-fastq-to-tdf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INDIR=/scratch/Shares/public/sread2024/data_files/day5/fastq/for_loops_fastq/ 
+OUTDIR=/Users/your_username/sr2024/day5/output
+
+# makes a new directory if it does not already exist
+mkdir -p ${OUTDIR}
+
+# loops through each file that end in .end1.fastq in the in directory
+for path_and_filename in ${INDIR}/*.end1.fastq
+do 
+# CODE TO OPERATE ON PATH_AND_FILENAME
+# first get JUST the filename prefix (not including path or .end1.fastq)
+FILENAME=`basename $pathandfilename .end1.fastq`
+echo $FILENAME
+
+# now run the sbatch with the script d5-fastq-to-bam.sbatch.
+# sbatch --export submits the script to the compute cluster with the variable values specified
+# clarify the variable values for the indir, rootname, and outdir
+# Which variable should change with each iteration the he loop? --> FILENAME?
+
+sbatch --export=indir=$INDIR,rootname=$FILENAME,outdir=$OUTDIR d5-fastq-to-tdf.sbatch
+done
+
+
+echo "DONE!"
