394a71f1dccaf454b9887f79f86a1e1ddf9e462b
Mary Allen <Mary.A.Allen@colorado.edu>
2025-07-14 09:58:21 -0600

    Adding making M and M and metadata

---
+++ projectB/day07/mnm_activity/mandm_makebowl.R
@@ -0,0 +1,33 @@
+
+
+totalMandMs = 10000
+mmbluepercent = 0.24
+mmorangepercent = 0.2
+mmgreenpercent = 0.16
+mmyellowpercent = 0.14
+mmredpercent = 0.13
+mmbrownpercent = 0.13
+
+#big marsh
+#tiny marsh
+#blue
+#brown
+#orange
+#yellow
+#red
+#green
+#orange_sktl
+#green_sktl
+#red_sktl
+#yellow_sktl
+#purple_sktl
+#total M and Ms
+
+MandMcolors = c("mmblue", "mmorange", "mmgreen", "mmyellow", "mmgreen", "mmyellow", "mmbrown")
+MandMspecents = rbind(mmbluepercent,mmorangepercent,mmgreenpercent,mmyellowpercent, mmredpercent, mmbrownpercent)
+
+#24% blue, 20% orange, 16% green, 14% yellow, 13% red, and 13% brown
+
+makeabowl <- function(MandMs, skittles, bigmarshmellows, tinymarshmellows) { # create a function with the name my_function
+  print("Hello World!")
+} 
