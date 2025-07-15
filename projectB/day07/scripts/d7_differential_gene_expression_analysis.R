8627d61830baa1c8159d0776cf9b9aa4976de070
Rutendo Sigauke <rsigauke@gmail.com>
2025-07-15 01:01:28 -0600

    Fix names, paths and typos in DESeq2 day7 script.

--- projectB/day07/scripts/d7_differential_gene_expression_analysis.R
+++ projectB/day07/scripts/d7_differential_gene_expression_analysis.R
@@ -9,14 +9,14 @@ library(DESeq2)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
-library(dyplr)
+
 ##########################################
 # Loading the 2 input files.             #
 ##########################################
 
 # 1. Load the metadata for the analysis
 
-conditionsTableFile <- "/PATH/TO/day7/Andrysik2017_samples.tsv"
+conditionsTableFile <- "/PATH/TO/srworkshop/projectB/day07/data/Andrysik2017_samples.tsv"
 conditionsTable <- read.table(conditionsTableFile,
                               sep = "\t",
                               header= TRUE)
@@ -25,7 +25,7 @@ conditionsTable
 
 # 2. Load the raw counts
 
-geneCountsTableFile <- "/PATH/TO/day7/Andrysik2017_counts.tsv"
+geneCountsTableFile <- "/PATH/TO/srworkshop/projectB/day07/data/Andrysik2017_counts.tsv"
 geneCountsTable <- read.table(geneCountsTableFile,
                               header=TRUE,
                               row.names = "GeneID",
@@ -77,7 +77,12 @@ hist(normcounts$SRR4098433.sorted.bam)
 boxplot(normcounts)
 
 rld <- rlog(DEdds)
-DESeq2::plotPCA(rld, intgroup=c("replicate"))
+
+DESeq2::plotPCA(rld,
+                intgroup=c("replicate"),
+                ntop=500)
+
+DESeq2::plotPCA(rld, intgroup=c("condition"))
 
 
 ################### ######################
@@ -124,14 +129,25 @@ plotCounts(DEdds, gene,
 ##########################################
 # Plots Volcano plot.                    #
 ##########################################
-results_shrunk$threshold <- ifelse(results_shrunk$padj < alphaValue, "significant", "not_significant")
-ggplot(results_shrunk) +
-  geom_point(aes(x = log2FoldChange, y = -log10(padj), color = threshold)) +
-  ggtitle("Volcano Plot") +
-  xlab("log2 fold change") + 
-  ylab("-log10 adjusted p-value") +
-  theme_classic()
 
+#add a column where we specify the significant vs. non-sig genes
+results_shrunk$threshold <- ifelse(results_shrunk$padj < alphaValue, "significant", "not_significant")
+
+#use the newly added column to color genes in volcano plot
+ggplot(results_shrunk) +
+  geom_point(shape=21,
+             aes(x = log2FoldChange, y = -log10(padj),
+                 fill = threshold), color="white", size=2) +
+  labs(title = "Volcano Plot", 
+       x = "log2 fold change",
+       y = "-log10 adjusted p-value",
+       fill = "Threshold") +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5, face='plain'),
+        title = element_text(size = 16), 
+        axis.title = element_text(size = 14),
+        axis.text.y = element_text(size = 12),
+        axis.text.x = element_text(size = 12))
 
 #########################################
 # Sorts results by padj.                #
@@ -152,14 +168,14 @@ write.table(results_shrunk_sig,
             quote = FALSE, 
             row.names = TRUE,
             col.names = TRUE,
-            paste(outdir, "Andrysik2017_RNAseq_Nutlin_results.tsv", sep="")
+            paste(outdir, "Andrysik2017_RNAseq_Nutlin_results.tsv", sep=""))
 
 res_shrink_expressed <- as.data.frame(results_shrunk)
 res_shrink_expressed <- res_shrink_expressed[!is.na(res_shrink_expressed$padj),]
 
 
-write.csv(rownames(res_shrink_expressed), file = paste0(outdir,"backgroundgenes.csv"),row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.csv(rownames(results_shrunk_sig), file = paste0(outdir,"siggenes.csv"),row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(rownames(res_shrink_expressed), file = paste0(outdir,"backgroundgenes.csv"),row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(rownames(results_shrunk_sig), file = paste0(outdir,"siggenes.csv"),row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 rnkdf <- tibble(gene = rownames(results),
 				rnk = -log(results$pvalue) * sign(results$log2FoldChange)) %>%
@@ -174,6 +190,7 @@ write.table(rnkdf, file = paste0(outdir,"deseq_res_for_gsea.rnk"),
 #########################################
 # EXTRA: Creating Heatmaps              #
 #########################################
+#install.packages("pheatmap")
 library(pheatmap)
 
 select <- order(rowMeans(counts(DEdds, normalized = TRUE)),
@@ -192,3 +209,11 @@ pheatmap(normcounts[select,],
          show_rownames = TRUE,
          cluster_cols = FALSE,
          annotation_col = columns_for_heatmap)
+
+########################################
+##Lastly, let us print information on ##
+##the R session we have been running. ##
+########################################
+#?sessionInfo #Get and report version information about R, the OS and attached or loaded packages.
+sessionInfo()
+
